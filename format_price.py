2f34dbab62de22c2b46b4d5cbf6d254ca8da39e0
Sergey Morozov <sergeymorozov@MacBook-Pro-Sergey.local>
2018-04-24 12:04:09 +0300

    tests are written

--- format_price.py
+++ format_price.py
@@ -1,5 +1,32 @@
-def format_price(price):
-    pass
+import re
+import argparse
 
-if __name__ == '__main__':
-    pass
+
+def format_price(price):
+    if not re.match(r"^\d+\.?\d*$", str(price)):
+        raise ValueError(
+            "Wrong input, expected number, but {} was given".format(price)
+        )
+    price = float(price)
+    if price.is_integer():
+        format_string = ",.0f"
+    else:
+        format_string = ",.2f"
+    return format(price, format_string).replace(",", " ")
+
+
+def get_input_argument_parser():
+    parser = argparse.ArgumentParser("Format number to price")
+    parser.add_argument(
+        "-n",
+        "--number",
+        required=True,
+        help="Number to format"
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_input_argument_parser()
+    args = parser.parse_args()
+    print(format_price(args.number))
--- tests.py
+++ tests.py
@@ -1,1 +1,63 @@
-# TODO place your tests here
+import unittest
+from .format_price import format_price
+
+
+class FormatPriceTests(unittest.TestCase):
+    def test_should_format_short_number(self):
+        price = 123
+        expected = "123"
+        result = format_price(price)
+        self.assertEqual(expected, result)
+
+    def test_should_format_long_number(self):
+        price = 1234567890
+        expected = "1 234 567 890"
+        result = format_price(price)
+        self.assertEqual(expected, result)
+
+    def test_should_format_number_with_float_part(self):
+        price = 1234.56
+        expected = "1 234.56"
+        result = format_price(price)
+        self.assertEqual(expected, result)
+
+    def test_should_format_number_with_zero_float_part(self):
+        price = 1.0000
+        expected = "1"
+        result = format_price(price)
+        self.assertEqual(expected, result)
+
+    def test_should_format_zero(self):
+        price = 0
+        expected = "0"
+        result = format_price(price)
+        self.assertEqual(expected, result)
+
+    def test_should_format_string(self):
+        price = "1234567890.01"
+        expected = "1 234 567 890.01"
+        result = format_price(price)
+        self.assertEqual(expected, result)
+
+    def test_should_return_error_for_incorrect_symbols(self):
+        with self.assertRaises(ValueError):
+            price = "!.@#"
+            format_price(price)
+
+    def test_should_return_error_for_double_float_part(self):
+        with self.assertRaises(ValueError):
+            price = "123.56.78"
+            format_price(price)
+
+    def test_should_return_error_for_letters(self):
+        with self.assertRaises(ValueError):
+            price = "one hundred twenty three"
+            format_price(price)
+
+
+
+
+
+
+
+
